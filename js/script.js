1cf346559ddd019ee6947230e4410cb035d92536
Vasilii Zalizniak <vzalizniak31@gmail.com>
2021-10-21 16:24:43 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
-const userNumber = +prompt(`Enter any number, please:`);
+const userNumber = prompt(`Enter any number, please:`);
 const exponentUserNumber = prompt(
   `Enter the exponent of the number you entered earlier, please:`
 );
 
-function getNumberInExponent(number, exponentNumb) {
-  if (isNaN(number) || isNaN(exponentNumb)) {
-    return `You entered not a number.`;
-    /* Ниже в else if () {} обработал значение переменной, отвечающей за возведение в степень,
-       как строку, чтобы добраться до пустой строки (нажатие "ок" в prompt без ввода данных)
-       или до "null" (escape или отмена) и, в случае успешного выполнения условия,
-       применить параметр по умолчанию, равный 1. Использовал вложенность функции, так как
-       иначе не смог обработать "0" (а такое значение возведенияв степень должно быть) и
-       отдельно: "null" и пустую строку. */
-  } else if (exponentNumb === "" || exponentNumb === null) {
-    return getNumberInExponent(number, (exponentNumb = 1));
-  } else {
-    return Math.pow(number, exponentNumb);
-  }
+function getNumberInExponent(number, exponentNumb = 1) {
+  if (number === null || isNaN(+number) || number.trim() === "")
+    return "number is not valid";
+
+  if (exponentNumb === null || isNaN(+exponentNumb) || exponentNumb === "")
+    return "exponentNumb is not valid";
+  return Math.pow(number, exponentNumb);
 }
 
 const result = getNumberInExponent(userNumber, exponentUserNumber);
-alert(result);
+console.log(result);
